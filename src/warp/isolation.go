d9c17adc968b48002acf24cc268fe39223b68864
AlexZander85 <344792@gmail.com>
2026-07-31 00:46:08 +0500

    warp: isolate outer inner camouflage state

---
+++ src/warp/isolation.go
@@ -0,0 +1,17 @@
+package warp
+
+type InstanceState struct {
+	InstanceID        string
+	Mark              uint32
+	SessionGeneration string
+	Active            bool
+}
+type IsolationReport struct {
+	Outer, Inner             InstanceState
+	ParentLinkValid          bool
+	InnerRevokedBeforeParent bool
+}
+
+func (r IsolationReport) Valid() bool {
+	return r.Outer.InstanceID != "" && r.Inner.InstanceID != "" && r.Outer.InstanceID != r.Inner.InstanceID && r.Outer.Mark != r.Inner.Mark && r.ParentLinkValid
+}
---
+++ src/warp/isolation_test.go
@@ -0,0 +1,14 @@
+package warp
+
+import "testing"
+
+func TestOuterInnerStateCannotCrossAuthorize(t *testing.T) {
+	r := IsolationReport{Outer: InstanceState{InstanceID: "o", Mark: 1}, Inner: InstanceState{InstanceID: "i", Mark: 2}, ParentLinkValid: true}
+	if !r.Valid() {
+		t.Fatal(r)
+	}
+	r.Inner.Mark = 1
+	if r.Valid() {
+		t.Fatal("cross-instance mark accepted")
+	}
+}
