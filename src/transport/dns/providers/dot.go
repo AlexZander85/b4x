c307e6e53627de9058b5a9138f9eb5dd646082ce
AlexZander85 <344792@gmail.com>
2026-09-15 19:48:12 +0500

    dns: reject unproven DoT negative answers

--- src/transport/dns/providers/dot.go
+++ src/transport/dns/providers/dot.go
@@ -160,6 +160,9 @@ func (p *DoTProvider) Resolve(ctx context.Context, prepared dnspath.PreparedDNSP
 	if err != nil {
 		return dnspath.DNSResponse{}, err
 	}
+	if err := validateProductionResponse(resp, obs); err != nil {
+		return dnspath.DNSResponse{}, err
+	}
 	return dnspath.DNSResponse{Payload: resp, Fingerprint: fp, RCode: obs.RCode, Latency: latency, ResponseCount: 1}, nil
 }
 
